960d9e73f59091f3575c48412a9b203a4ed3cda3
Ana7293 <117599582+Ana7293@users.noreply.github.com>
2023-02-20 19:31:17 +0500

    Lesson2\2

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args){         //точка входа в программу
+
+        System.out.println("Hello, World!");        //вызов метода
+        System.out.println("Привет, Настя");
+        System.out.println("Привет, Саша");
+        System.out.println("Привет, Андрей");
+        System.out.println("Привет, Тихон");
+        System.out.println("Привет, Антон");
+        System.out.println("Привет, Костя");
+    }
+
+}

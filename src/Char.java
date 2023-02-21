0d52590f98bb93b36b6ac4f4af399448d8cf2de3
ana7293 <ana7293nik@gmail.com>
2023-02-22 00:08:31 +0500

    Lesson2\7

---
+++ src/Char.java
@@ -0,0 +1,8 @@
+public class Char {
+    public static void main(String[] args) {
+        char x = 88;
+        char y = 'x';
+        y++;
+        System.out.println("Значение переменной x = " + x + " , " + "а значение переменной y = " + y);
+    }
+}

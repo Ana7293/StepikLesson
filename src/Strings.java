e18f2e232fe53ee3eedd72877fc52995375eef53
ana7293 <ana7293nik@gmail.com>
2023-02-22 01:22:28 +0500

    Lesson2\9

---
+++ src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        String s = "Здравствуй, мой юный друг!";
+        System.out.println(s);
+
+        String firstName = "Анастасия";
+        String lastName = "Николаева";
+        int age = 33;
+        String name = firstName + " " + lastName + ",\nВозраст (\"полных\" лет): " + age;
+        System.out.println(name);
+    }
+}
